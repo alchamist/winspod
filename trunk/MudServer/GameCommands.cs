17d9cefcb0c235fd99ef07333436d76e4d77fc2e
alchamist <alchamist@109aa2fe-2914-41dd-ed2f-febdd37c6211>
2014-02-03 09:46:17 +0000

    Fixed issue with ASCII control characters sent from some programs (e.g. TinyFugue)

---
+++ trunk/MudServer/GameCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MudServer
+{
+    public partial class Connection
+    {
+        public void cmdMSweep(string message)
+        {
+            sendToUser("Sorry, this command hasn't been implimented yet", true, false, false);
+        }
+    }
+}
